570aaca679cb632d2b10eaa03611edaa8347351c
Dylan Katz <dylanjaredkatz@gmail.com>
2025-03-26 17:25:55 -0700

    better error handling

---
+++ src/routes/[eventId]/edit/+error.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<svelte:head>
+	<title>Error Editing Event - Event Planner</title>
+</svelte:head>
+
+<h2>Error Editing Event</h2>
+<p>{page.error}</p>
+
+<div class="grid grid-cols-2 gap-2">
+	<a class="btn" href={'/' + page.params.eventId}>Return to Event</a>
+	<a class="btn" href="/">Return Home</a>
+</div>
---
+++ src/routes/newevent/+error.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<svelte:head>
+	<title>Error Adding Event - Event Planner</title>
+</svelte:head>
+
+<h2>Error Adding Event</h2>
+<p>{page.error}</p>
+
+<a class="btn" href="/">Return Home</a>
